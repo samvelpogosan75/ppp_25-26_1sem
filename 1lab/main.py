d148a1840dac7fe2e64bd10b59745dc67ef35ee1
samvelpogosan75 <samvelpogosan750@gmail.com>
2025-10-20 11:47:43 +0300

    Update main.py

--- 1lab/main.py
+++ 1lab/main.py
@@ -1,4 +1,25 @@
 
 
 if __name__ == "__main__":
-    pass # Ваш код здесь
+hall = [[0]*7 for g in range(5)]
+
+def show():
+    for y in hall:
+        print(y)
+ 
+def book_seats(n):
+    for i, y in enumerate(hall):
+        for j in range(len(y)-n+1):
+            if sum(y[j:j+n]) == 0:
+                for k in range(n):
+                    y[j+k] = 1
+                print(f"Ряд {i+1}, места {j+1}-{j+n}")
+                return
+    print("Нет мест")
+
+# Пример
+show()
+book_seats(4)
+show()
+book_seats(7)
+show()
