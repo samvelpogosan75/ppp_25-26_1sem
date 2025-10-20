79069d7289a7d2457b461e09a9afb5d18d066608
samvelpogosan75 <samvelpogosan750@gmail.com>
2025-10-20 19:55:31 +0300

    Update main.py

--- 2lab/main.py
+++ 2lab/main.py
@@ -1,4 +1,18 @@
 
 
 if __name__ == "__main__":
-    pass # Ваш код здесь
+def brackets(text):
+    stack = []
+    for index, symbol in enumerate(text, 1):
+        if symbol in '([{':
+            stack.append(symbol)
+        elif symbol in ')]}':
+            if not stack:
+                return f"Ошибка на позиции {index}"
+            last = stack.pop()
+            if last + symbol not in ['()', '[]', '{}']:
+                return f"Ошибка на позиции {index}"
+    if stack:
+        return "Ошибка: не все скобки закрыты"
+    return "ok"
+print(brackets("(a)(b(c[d{e{r}f{g}s}w]r)tasd)"))
