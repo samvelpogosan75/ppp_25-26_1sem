a48abe72614007330fe7225d3c5a7a74098afd5f
samvelpogosan75 <samvelpogosan750@gmail.com>
2025-12-01 10:23:11 +0300

    Update main.py

--- 3lab/main.py
+++ 3lab/main.py
@@ -1,4 +1,32 @@
+log = []
+
+def calc(expr):
+    expr = expr.strip()
+    log.append("вычисляем: " + expr)
+
+    if "(" not in expr:
+        result = eval(expr)     # самый простой способ
+        log.append(f"{expr} = {result}")
+        return result
+
+    left = expr.rfind("(")
+    right = expr.find(")", left)
+
+    inside = expr[left+1:right]
+    log.append("нашли скобки: " + inside)
+
+    value = calc(inside)
+
+    new_expr = expr[:left] + str(value) + expr[right+1:]
+    log.append("после подстановки: " + new_expr)
+
+    return calc(new_expr)
 
 
-if __name__ == "__main__":
-    pass # Ваш код здесь
+expr = "(2 + (3 * (4 - 1)))"
+res = calc(expr)
+
+print("Результат:", res)
+print("\nШаги:")
+for s in log:
+    print(s)
